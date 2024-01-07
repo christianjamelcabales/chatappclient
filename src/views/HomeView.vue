e3a007e6234cf11ae58cdec80609d3cc745d0c42
CJ Cabales <christianjamelcabales@gmail.com>
2024-01-07 13:53:49 +0800

    changed title

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
   <headerSection />
   <div class="container text-center pt-5">
-      <h3>Welcome to <strong>Onlingers</strong> </h3>
-      <h4>Gateway to meet strangers online </h4>
+      <h3>Intoducing <strong>StrangersLink</strong> </h3>
+      <h4>Discover Online Connections </h4>
       
       <router-link to="/chat"><strong>Chat Now</strong></router-link>
-<br>
+      <br>
       <img src="../../public/cjlogonew.png" class="mt-5" alt="">
       <p>&copy; 2024. All Rights Reserved </p>
   </div>
