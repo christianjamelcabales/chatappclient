6ddb939226338c54230838d01100b32fa97aa95d
Christian Jamel Cabales <cjcabales@yondu.com>
2025-05-06 15:31:30 +0800

    changed Intro

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,13 @@
   <headerSection />
   <div class="container text-center pt-5">
       <h3>Introducing <strong>Yukimeet</strong> </h3>
-      <h4><i>Discover Connections in the Online Space</i></h4>
+      <h4><i>Jump into real-time conversations with strangers online. <br>
+        No sign-up, no hassle. Just pure connection.</i></h4>
       <div class="mt-5">
-        <router-link to="/chat"><strong>Chat Now</strong></router-link>
+        <router-link to="/chat"><strong>[Chat Now]</strong></router-link>
       </div>
       <br>
+      
       <div class="fixed-bottom mb-5">
         <img src="../../public/cjlogonew2.png" class="mt-5" alt="">
         <p>&copy; 2024. All Rights Reserved </p>
